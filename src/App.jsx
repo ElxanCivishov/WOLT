833ad54d69d22ecfdc81a8e4575ec8f0f2a3df1a
Elxan Civishov <elxan.civishov2000@gmail.com>
2023-03-03 22:55:46 +0400

    create repository

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <h1 class="text-4xl bg-slate-500 font-bold underline">Hello world!</h1>
+  );
+}
+
+export default App;
